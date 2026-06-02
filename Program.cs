6214815411ddba479e2bd25fcb238bf42e8dfedc
opilane <opilane@HP-CZC2349HT2>
2026-06-02 09:52:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace MaduUss_Puhtejev
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Seadistame konsooli akna
+            Console.WindowHeight = 20;
+            Console.WindowWidth = 50;
+            Console.CursorVisible = false;
+
+            // Loome objektid
+            Uss uss = new Uss(10, 10, 3);
+            Toit toit = new Toit(Console.WindowWidth, Console.WindowHeight);
+
+            // MÄNGU TSÜKKEL
+            while (true)
+            {
+                // 1. Sisendi lugemine (kas kasutaja vajutas nooleklahvi?)
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo klahv = Console.ReadKey(true);
+                    // Muudame suunda, aga väldime tagurdamist
+                    if (klahv.Key == ConsoleKey.UpArrow && uss.PraeguneSuund != Suund.Alla)
+                        uss.PraeguneSuund = Suund.Üles;
+                    else if (klahv.Key == ConsoleKey.DownArrow && uss.PraeguneSuund != Suund.Üles)
+                        uss.PraeguneSuund = Suund.Alla;
+                    else if (klahv.Key == ConsoleKey.LeftArrow && uss.PraeguneSuund != Suund.Paremale)
+                        uss.PraeguneSuund = Suund.Vasakule;
+                    else if (klahv.Key == ConsoleKey.RightArrow && uss.PraeguneSuund != Suund.Vasakule)
+                        uss.PraeguneSuund = Suund.Paremale;
+                }
+
+                // 2. Objektide uuendamine
+                uss.Liigu();
+                Punkt pea = uss.HangiPea();
+
+                // 3. Kas uss sõi toidu ära?
+                if (pea.X == toit.Asukoht.X && pea.Y == toit.Asukoht.Y)
+                {
+                    uss.Kasva();
+                    toit.LooUusToit();
+                }
+
+                // 4. Kas uss põrkas vastu seina? (Lihtne mängu lõpu kontroll)
+                if (pea.X <= 0 || pea.X >= Console.WindowWidth - 1 ||
+                    pea.Y <= 0 || pea.Y >= Console.WindowHeight - 1)
+                {
+                    Console.SetCursorPosition(15, 10);
+                    Console.WriteLine("MÄNG LÄBI!");
+                    break; // Murrame tsüklist välja
+                }
+
+                // Mängu kiirus (mida väiksem number, seda kiirem uss)
+                Thread.Sleep(150);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Punkt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaduUss_Puhtejev
+{
+    public enum Suund
+    {
+        Üles, Alla, Vasakule, Paremale
+    }
+
+    public class Punkt
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Sümbol { get; set; } // Näiteks '*' ussi jaoks, '@' toidu jaoks
+
+        public Punkt(int x, int y, char sümbol)
+        {
+            X = x;
+            Y = y;
+            Sümbol = sümbol;
+        }
+
+        // See meetod joonistab punkti konsooli õigesse kohta
+        public void Joonista()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(Sümbol);
+        }
+
+        // Puhastab eelmise asukoha (kui uss liigub edasi)
+        public void Kustuta()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(' '); // Kirjutame tühiku peale
+        }
+    }
+}
